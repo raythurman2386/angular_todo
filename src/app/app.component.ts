53bc86efcad3894b0838441561ca485bb4e2b517
raythurman2386 <raymondthurman5@gmail.com>
2023-07-18 10:27:31 -0500

    corrected empty array error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,17 @@ export class AppComponent {
   filter: "all" | "active" | "done" = "all";
 
 
-  allItems = [
-    { description: "eat", done: true },
-    { description: "sleep", done: false },
-    { description: "play", done: false },
-    { description: "coffee", done: true },
-  ]
+  allItems: Item[] = []
 
   get items() {
     if (this.filter === "all") {
       return this.allItems
     }
-    return this.allItems.filter((item) => this.filter === "done" ? item.done : !item.done)
+    if (this.allItems.length > 0) {
+      return this.allItems.filter((item) => this.filter === "done" ? item.done : !item.done)
+    }
+
+    return this.allItems
   }
 
   addItem(description: string) {
